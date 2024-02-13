3e5da8dbff3c3150b66bfcaccf48dfc40d9e3085
Divyesh Mepal <divyeshm@zignuts.com>
2024-02-13 19:16:45 +0530

    Refactor app to use Timeline component for better organization Also, import Bulma CSS framework for styling.

---
+++ src/components/Timeline.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue"
+
+const periods = ["Today", "This Week", "This Month"] as const;
+
+type Period = typeof periods[number]
+
+const selectedPeriod = ref<Period>("Today");
+
+function selectPeriod(period: Period) {
+  selectedPeriod.value = period;
+}
+</script>
+
+<template>
+  <div>
+    {{ selectedPeriod }}
+    <nav class="is-primary panel">
+      <span class="panel-tabs">
+        <a v-for="period of periods" :key="period" @click="selectPeriod(period)"
+          :class="{ 'is-active': period === selectedPeriod }"> {{ period }}
+        </a>
+      </span>
+    </nav>
+  </div>
+</template>
+
+<style></style>

4f534fefba90112d44d1d7727feb09b6d1fdeaaf
Divyesh Mepal <divyeshm@zignuts.com>
2024-02-14 11:46:33 +0530

    Add Luxon for date manipulation and display posts based on selected period.

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-import { ref } from "vue"
+import { ref, computed } from "vue"
+import { Post, today, thisMonth, thisWeek } from "../posts";
+import { DateTime } from "luxon";
 
 const periods = ["Today", "This Week", "This Month"] as const;
 
@@ -10,17 +12,42 @@ const selectedPeriod = ref<Period>("Today");
 function selectPeriod(period: Period) {
   selectedPeriod.value = period;
 }
+
+const posts = computed(() => {
+  return [
+    today,
+    thisWeek,
+    thisMonth
+  ].map(post => {
+    return {
+      ...post,
+      created: DateTime.fromISO(post.created)
+    }
+  }).filter(post => {
+    if (selectedPeriod.value === "Today") {
+      return post.created >= DateTime.now().minus({ day: 1 })
+    }
+    if (selectedPeriod.value === "This Week") {
+      return post.created >= DateTime.now().minus({ week: 1 })
+    }
+    return post
+  })
+})
 </script>
 
 <template>
   <div>
-    {{ selectedPeriod }}
     <nav class="is-primary panel">
       <span class="panel-tabs">
         <a v-for="period of periods" :key="period" @click="selectPeriod(period)"
           :class="{ 'is-active': period === selectedPeriod }"> {{ period }}
         </a>
       </span>
+
+      <a href="" v-for="post of posts" :key="post.id" class="panel-block">
+        <a href="">{{ post.title }}</a>
+        <div>{{ post.created.toFormat('dd-MMM-y') }}</div>
+      </a>
     </nav>
   </div>
 </template>
